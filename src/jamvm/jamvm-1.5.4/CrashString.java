2d4698ef21e980ae7740bfd127a9b19efb28089c
Maiki Buffet <maiki.buffet@cpca.pucrs.br>
2016-01-29 14:36:11 -0200

    fixing test files

---
+++ src/jamvm/jamvm-1.5.4/CrashString.java
@@ -0,0 +1,17 @@
+public class CrashString {
+	public static String str = "oi";
+	public static int x = 0;
+	public static Object lock = new Object();
+	public static void main(String[] args){
+		str = new String("\\m/ NA VEIA");
+		System.out.println(str);
+		synchronized(lock) {
+			while(true) {
+				str = new String("int = " + x);
+				x++;
+				System.out.println(str);
+			}
+		}
+
+	}
+}
