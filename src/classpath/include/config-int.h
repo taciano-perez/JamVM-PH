1494f99008d6916ff1b2dc3d1bc338406c4e1810
Taciano Perez <taciano.perez@hp.com>
2016-06-19 15:57:28 +0000

    removing unnecessary files

--- src/classpath/include/config-int.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CLASSPATH_INCLUDE_CONFIG_INT_H
-#define _CLASSPATH_INCLUDE_CONFIG_INT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "classpath 0.98"
-/* generated using gnu compiler gcc (Ubuntu 5.2.1-22ubuntu2) 5.2.1 20151010 */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
