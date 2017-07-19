3005731e32e44f95146e9e97bc2ed3049c17fc4d
Taciano Perez <taciano.perez@hp.com>
2017-07-19 16:55:48 -0300

    Saving files before refreshing line endings

--- src/classpath/compile.sh
+++ src/classpath/compile.sh
@@ -2,6 +2,7 @@
 clear
 sudo make uninstall
 sudo make clean
+autoreconf --force --install --symlink
 ./configure --disable-Werror --disable-plugin --disable-gtk-peer --disable-gconf-peer --disable-tools --disable-examples
 make
 sudo make install
